e4e300231fd6ddd16793dd794fc64503e86d1820
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2022-02-19 21:49:23 +0100

    v1.0.3

--- HypeDataParser.js
+++ HypeDataParser.js
@@ -16,112 +16,112 @@ if("HypeDataParser" in window === false) window['HypeDataParser'] = (function ()
 
 	var _extensionName = 'Hype Data Parser';
 
-    /**
+	/**
 	 * This function parses a CSV string into an array structur
-     * Given a second paramter options of type object, default options can be overriden
+	 * Given a second paramter options of type object, default options can be overriden
 	 *      * fieldSeparator defaults to ','
-     *      * rowSeparator defaults to '\n'
-     *      * quot defaults to '"'
-     *      * removeHead defaults to false and allows ignoring the first row
-     *      * trim defaults to false
-     * 
+	 *      * rowSeparator defaults to '\n'
+	 *      * quot defaults to '"'
+	 *      * removeHead defaults to false and allows ignoring the first row
+	 *      * trim defaults to false
+	 * 
 	 * @param {String} text This is the text to consider as CSV
 	 * @param {Object} options This object can be used to override defaults
-     * @return {Array} Returns an array of rows with nested arrays of field data
+	 * @return {Array} Returns an array of rows with nested arrays of field data
 	 */
-     csvToArray = function (text, options) {
-        var defaultOptions = {
-            'fieldSeparator': ';',
-            'rowSeparator': '\n',
-            'quot': '"',
-            'removeHead': false,
-            'trim': false
-        }
-        var o = typeof(options) == 'object'? Object.assign(defaultOptions, o) : defaultOptions;
-        var a = [['']];
+	 csvToArray = function (text, options) {
+		var defaultOptions = {
+			'fieldSeparator': ';',
+			'rowSeparator': '\n',
+			'quot': '"',
+			'removeHead': false,
+			'trim': false
+		}
+		var o = typeof(options) == 'object'? Object.assign(defaultOptions, o) : defaultOptions;
+		var a = [['']];
 
-        for (var r = f = p = q = 0; p < text.length; p++) {
-            switch (c = text.charAt(p)) {
-        
-                case o.quot:
-                    if (q && text.charAt(p + 1) == o.quot) {
-                        a[r][f] += o.quot;
-                        ++p;
-                    } else {
-                        q ^= 1;
-                    }
-                    break;
-                
-                case o.fieldSeparator:
-                    if (!q) {
-                        if (o.trim) {
-                            a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-                        }
-                        a[r][++f] = '';
-                    } else {
-                        a[r][f] += c;
-                    }
-                    break;
-                
-                case o.rowSeparator.charAt(0):
-                    if (!q && (!o.rowSeparator.charAt(1) || (o.rowSeparator.charAt(1) && o.rowSeparator.charAt(1) == text.charAt(p + 1)))) {
-                        if (o.trim) {
-                            a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-                        }
-                        a[++r] = [''];
-                        a[r][f = 0] = '';
-                        if (o.rowSeparator.charAt(1)) {
-                            ++p;
-                        }
-                    } else {
-                        a[r][f] += c;
-                    }
-                    break;
-                
-                default:
-                    a[r][f] += c;
-                    break;
-            }
-        }
+		for (var r = f = p = q = 0; p < text.length; p++) {
+			switch (c = text.charAt(p)) {
+		
+				case o.quot:
+					if (q && text.charAt(p + 1) == o.quot) {
+						a[r][f] += o.quot;
+						++p;
+					} else {
+						q ^= 1;
+					}
+					break;
+				
+				case o.fieldSeparator:
+					if (!q) {
+						if (o.trim) {
+							a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+						}
+						a[r][++f] = '';
+					} else {
+						a[r][f] += c;
+					}
+					break;
+				
+				case o.rowSeparator.charAt(0):
+					if (!q && (!o.rowSeparator.charAt(1) || (o.rowSeparator.charAt(1) && o.rowSeparator.charAt(1) == text.charAt(p + 1)))) {
+						if (o.trim) {
+							a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+						}
+						a[++r] = [''];
+						a[r][f = 0] = '';
+						if (o.rowSeparator.charAt(1)) {
+							++p;
+						}
+					} else {
+						a[r][f] += c;
+					}
+					break;
+				
+				default:
+					a[r][f] += c;
+					break;
+			}
+		}
 
-        if (o.removeHead) {
-            a.shift()
-        }
+		if (o.removeHead) {
+			a.shift()
+		}
 
-        if (a[a.length - 1].length < a[0].length) {
-            a.pop()
-        }
+		if (a[a.length - 1].length < a[0].length) {
+			a.pop()
+		}
 
-        return a;
-    }
+		return a;
+	}
 
-    /**
+	/**
 	 * This function uses csvToArray (see function description), but converts 
-     * the basic array into an object with keys based on the header.
-     * Because that is the case the removeHead option cannot be overriden and a header
-     * should be present in the CSV for this to work properly.
-     * 
+	 * the basic array into an object with keys based on the header.
+	 * Because that is the case the removeHead option cannot be overriden and a header
+	 * should be present in the CSV for this to work properly.
+	 * 
 	 * @param {String} text This is the text to consider as CSV
 	 * @param {Object} options This object can be used to override defaults
-     * @return {Array} Returns an array of rows with nested objects containing named fields
+	 * @return {Array} Returns an array of rows with nested objects containing named fields
 	 */
-    function csvToObject(text, options){
-        var rows = csvToArray(text, Object.assign(options, {
-            removeHead: false
-        }));
-        var headers = rows.shift();
-        var data = [];
-        
-        rows.forEach(function(row){
-            var obj = {}
-            row.forEach(function(cell, i){
-                obj[ headers[i] ] = cell;
-            });
-            data.push(obj);
-        });
-        
-        return data;
-    }
+	function csvToObject(text, options){
+		var rows = csvToArray(text, Object.assign(options, {
+			removeHead: false
+		}));
+		var headers = rows.shift();
+		var data = [];
+		
+		rows.forEach(function(row){
+			var obj = {}
+			row.forEach(function(cell, i){
+				obj[ headers[i] ] = cell;
+			});
+			data.push(obj);
+		});
+		
+		return data;
+	}
 
 	/**
 	 * @typedef {Object} HypeDataParser
@@ -132,7 +132,7 @@ if("HypeDataParser" in window === false) window['HypeDataParser'] = (function ()
 	 var HypeDataParser = {
 		version: '1.0.3',
 		csvToArray: csvToArray,
-        csvToObject: csvToObject,
+		csvToObject: csvToObject,
 	};
 
 	/** 
