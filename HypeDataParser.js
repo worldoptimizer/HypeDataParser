be7947497ea8abd9f191eb86d593d1fc0589477d
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2022-02-20 15:34:03 +0100

    v1.0.4

--- HypeDataParser.js
+++ HypeDataParser.js
@@ -1,8 +1,9 @@
 /*!
-Hype Data Parser 1.0.3
+Hype Data Parser 1.0.4
 copyright (c) 2022 Max Ziebell, (https://maxziebell.de). MIT-license
 Based on csvToArray from Daniel Tillin 2011-2013
 http://code.google.com/p/csv-to-array/
+https://leanylabs.com/blog/js-csv-parsers-benchmarks/
 */
 
 /*
@@ -11,48 +12,73 @@ http://code.google.com/p/csv-to-array/
 * 1.0.1 Added minified version
 * 1.0.2 Fixed minor typographic error Seperator to Separator
 * 1.0.3 Refactored head to removeHead, added forced removeHead false on csvToObject
+* 1.0.4 Refactored some more aspects of this CSV parser by Daniel Tillin
 */
 if("HypeDataParser" in window === false) window['HypeDataParser'] = (function () {
 
 	var _extensionName = 'Hype Data Parser';
 
+
+	function count(str, c) { 
+		var result = 0;
+		for(var i = 0; i<str.length; i++) {
+			if(str[i]==c)result++;
+		}
+		return result;
+	};
+
+	function getLineBreakChar(str) {
+		const indexOfLF = str.indexOf('\n', 1);
+		if (indexOfLF === -1) {
+			if (str.indexOf('\r') !== -1) return '\r';
+			return '\n';
+		}
+		if (str[indexOfLF - 1] === '\r') return '\r\n';
+		return '\n';
+	}
+
 	/**
 	 * This function parses a CSV string into an array structur
 	 * Given a second paramter options of type object, default options can be overriden
-	 *      * fieldSeparator defaults to ','
-	 *      * rowSeparator defaults to '\n'
+	 *      * fSep defaults to ','
+	 *      * rSep defaults to '\n'
 	 *      * quot defaults to '"'
-	 *      * removeHead defaults to false and allows ignoring the first row
+	 *      * head defaults to false and allows ignoring the first row
 	 *      * trim defaults to false
 	 * 
 	 * @param {String} text This is the text to consider as CSV
 	 * @param {Object} options This object can be used to override defaults
 	 * @return {Array} Returns an array of rows with nested arrays of field data
 	 */
-	 csvToArray = function (text, options) {
-		var defaultOptions = {
-			'fieldSeparator': ';',
-			'rowSeparator': '\n',
+	function csvToArray(csv, o) {
+		var od = {
+			'fSep': count(csv,';') > count(csv, ',')? ';' : ',',
+			'rSep': getLineBreakChar(csv),
 			'quot': '"',
-			'removeHead': false,
+			'head': false,
 			'trim': false
 		}
-		var o = typeof(options) == 'object'? Object.assign(defaultOptions, o) : defaultOptions;
-		var a = [['']];
-
-		for (var r = f = p = q = 0; p < text.length; p++) {
-			switch (c = text.charAt(p)) {
-		
+		if (o) {
+			for (var i in od) {
+				if (!o[i]) o[i] = od[i];
+			}
+		} else {
+			o = od;
+		}
+		var a = [
+			['']
+		];
+		for (var r = f = p = q = 0; p < csv.length; p++) {
+			switch (c = csv.charAt(p)) {
 				case o.quot:
-					if (q && text.charAt(p + 1) == o.quot) {
+					if (q && csv.charAt(p + 1) == o.quot) {
 						a[r][f] += o.quot;
 						++p;
 					} else {
 						q ^= 1;
 					}
 					break;
-				
-				case o.fieldSeparator:
+				case o.fSep:
 					if (!q) {
 						if (o.trim) {
 							a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
@@ -62,53 +88,47 @@ if("HypeDataParser" in window === false) window['HypeDataParser'] = (function ()
 						a[r][f] += c;
 					}
 					break;
-				
-				case o.rowSeparator.charAt(0):
-					if (!q && (!o.rowSeparator.charAt(1) || (o.rowSeparator.charAt(1) && o.rowSeparator.charAt(1) == text.charAt(p + 1)))) {
+				case o.rSep.charAt(0):
+					if (!q && (!o.rSep.charAt(1) || (o.rSep.charAt(1) && o.rSep.charAt(1) == csv.charAt(p + 1)))) {
 						if (o.trim) {
 							a[r][f] = a[r][f].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 						}
 						a[++r] = [''];
 						a[r][f = 0] = '';
-						if (o.rowSeparator.charAt(1)) {
+						if (o.rSep.charAt(1)) {
 							++p;
 						}
 					} else {
 						a[r][f] += c;
 					}
 					break;
-				
 				default:
 					a[r][f] += c;
-					break;
 			}
 		}
-
-		if (o.removeHead) {
+		if (o.head) {
 			a.shift()
 		}
-
 		if (a[a.length - 1].length < a[0].length) {
 			a.pop()
 		}
-
 		return a;
 	}
+
 
 	/**
 	 * This function uses csvToArray (see function description), but converts 
 	 * the basic array into an object with keys based on the header.
 	 * Because that is the case the removeHead option cannot be overriden and a header
-	 * should be present in the CSV for this to work properly.
+	 * should be present in the CSV for text to work properly.
 	 * 
 	 * @param {String} text This is the text to consider as CSV
 	 * @param {Object} options This object can be used to override defaults
 	 * @return {Array} Returns an array of rows with nested objects containing named fields
 	 */
-	function csvToObject(text, options){
-		var rows = csvToArray(text, Object.assign(options, {
-			removeHead: false
-		}));
+	 function csvToObject(csv, o){
+
+		var rows = csvToArray(csv, o)
 		var headers = rows.shift();
 		var data = [];
 		
@@ -130,9 +150,11 @@ if("HypeDataParser" in window === false) window['HypeDataParser'] = (function ()
 	 * @property {Function} csvToObject Convert a CSV string into an object
 	 */
 	 var HypeDataParser = {
-		version: '1.0.3',
+		version: '1.0.4',
 		csvToArray: csvToArray,
 		csvToObject: csvToObject,
+		getLineBreakChar: getLineBreakChar,
+		count: count,
 	};
 
 	/** 
